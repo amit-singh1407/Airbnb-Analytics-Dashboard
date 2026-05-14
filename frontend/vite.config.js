cb81dd4357fa3d78a6149bb3f6756a8427dca3c9
Amit Kumar Singh <kumaramit.14singh@gmail.com>
2026-05-14 11:37:22 +0530

    Configure deployment and fix base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://github.com/amit-singh1407/Airbnb-Analytics-Dashboard",
+  base: "/Airbnb-Analytics-Dashboard/",
 })
